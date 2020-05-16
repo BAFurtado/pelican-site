359fd3888b55f7f9ceadc8cb1e3208508e306978
bafurtado <furtadobb@gmail.com>
2020-05-16 20:31:00 -0300

    init

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'B Furtado'
+SITENAME = 'Random tidibits, here and there'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Python.org', 'http://python.org/')
+         ,)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('twitter.com/furtadobb', 'Twitter'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
